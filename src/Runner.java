8330b1b03b4b3bd7dc853c8e8d5bbb2d29fd6039
unknown <cormacmchale1@gmail.com>
2019-02-02 09:06:03 +0000

    indentation only

--- src/Runner.java
+++ src/Runner.java
@@ -5,18 +5,19 @@ public class Runner {
 
 	public Runner() {
 		// TODO Auto-generated constructor stub
+		//pizza
 	}
 
 	public static void main(String[] args) {
 
 
 		ReadInput reader= new ReadInput();
-		//select file
 		Input file1 = reader.readFile(new File("in/a_example.in"));
-		file1.printPizza();
-		PizzaCutter pc = new PizzaCutter();	
+
+		PizzaCutter pc = new PizzaCutter();
 		List<Slice> l1 = pc.cut1(file1);
-		
+
+
 	}
 
 }
